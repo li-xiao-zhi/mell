b0f55a9090a8759ebc9986a9573a3f70c8cf1536
li-xiao-zhi <1365381506@qq.com>
2019-10-25 17:16:23 +0800

    创建项目

---
+++ mall_promotion/src/test/java/com/mr/mall/mall_promotion/MallPromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.mall.mall_promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallPromotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall_service/src/test/java/com/mr/mall/mall_service/MallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.mall.mall_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
