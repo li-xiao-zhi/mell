76643b682ccde4441d4c4565ee2feaffece09c9b
li-xiao-zhi <1365381506@qq.com>
2019-10-26 16:41:20 +0800

    更改

---
+++ mall_promotion/src/main/java/com/mr/mall/pojo/SmsFlashPromotion.java
@@ -0,0 +1,65 @@
+package com.mr.mall.pojo;
+
+import java.util.Date;
+
+public class SmsFlashPromotion {
+    private Long id;
+
+    private String title;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Integer status;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
