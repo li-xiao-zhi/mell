3623cbdd1975201390e586bf286193479203b968
li-xiao-zhi <1365381506@qq.com>
2019-10-26 17:22:38 +0800

    更改

--- mall_promotion/src/main/java/com/mr/mall/controller/SmsFlashPromotionController.java
+++ mall_promotion/src/main/java/com/mr/mall/controller/SmsFlashPromotionController.java
@@ -4,4 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SmsFlashPromotionController {
+
+    public String test(){
+        return  null;
+    }
 }
